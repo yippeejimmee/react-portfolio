58afef819c388db92d26f3228dea2529269621f9
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-08-28 15:28:42 -0700

    updated readme

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default function About() {
+export default function Resume() {
     return (
         
         <div className='aboutBar'>
-            <p> Greetings! My name is Jimmy Tran.  I'm an aspiring coder.  Molded and shape by 8bit games, I hope to one day leave my footprints in the videgame industry.</p>
         </div>
     )
 }
